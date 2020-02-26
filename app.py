f98e2b9cdc2db2bebeff26f91d597e262d8d0755
dswhitehouse <dave@davewhitehouse.co.uk>
2020-02-26 17:55:34 +0000

    Refined Widgets
    Signed-off-by: dswhitehouse <dave@davewhitehouse.co.uk>

--- app.py
+++ app.py
@@ -1,37 +1,43 @@
 import dash
 import plotly.express as px
 import dash_core_components as dcc
+import dash_table as dt
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 import base64
 import pandas as pd
 import plotly.io as pio
+import re
 
 pio.templates.default = "seaborn"
 logo = base64.b64encode(open("logos/cii-logo.png", 'rb').read()).decode('ascii')
 df = pd.read_csv("./testData/chatter.csv")
-stylesheets = [dbc.themes.SPACELAB]
-app = dash.Dash(__name__, external_stylesheets=stylesheets)
+app = dash.Dash("__name__")
 
-navbar = dbc.NavbarSimple(
-    children=[
-        dbc.NavItem(dbc.NavLink("Settings", href="#")),
-        dbc.NavItem(dbc.NavLink("Logout", href='#'))
-    ],
-    brand="Chatter Logger",
-    brand_href="#",
-    color="primary",
-    dark=True,
+navbar = dbc.Row(
+    dbc.Col(
+        dbc.NavbarSimple(
+            children=[
+                dbc.NavItem(dbc.NavLink("Settings", href="#")),
+                dbc.NavItem(dbc.NavLink("Logout", href='#'))
+            ],
+            brand="Chatter Logger",
+            brand_href="#",
+            color="primary",
+            fluid=True,
+            dark=True,
+        ), width=12
+    )
 )
 
 histo = dbc.Row(
     [
-        dbc.Col(
-            html.Div(
-                html.Img(src='data:image/png;base64,{}'.format(logo), height=200)
-            ),
-            width={"size": 2, "offset": 1}
-        ),
+        # dbc.Col(
+        #     html.Div(
+        #         html.Img(src='data:image/png;base64,{}'.format(logo), height=100)
+        #     ),
+        #     width={"size": 2, "offset": 1}
+        # ),
         dbc.Col(
             dcc.Graph(
                 id="main-histogram",
@@ -40,11 +46,24 @@ histo = dbc.Row(
                                     labels={"transactionTime": "Query Time",
                                             "username": "User"}),
             )
-            , width={"size": 8})
+            , width={"size": 12})
     ]
 )
 
-app.layout = html.Div([navbar, histo])
+dtable = dbc.Row(
+    dbc.Col(
+        dt.DataTable(
+            id="dtable",
+            columns=[{"name": i, "id": i} for i in df.columns if not re.search('lat.*|long.*', i)],
+            data=df.to_dict('records'),
+            page_action="native",
+            page_size=10
+        )
+        , width={'size': 10, "offset": 1}
+    )
+)
+
+app.layout = html.Div([navbar, histo, dtable])
 
 if __name__ == "__main__":
     app.run_server(debug=True)
