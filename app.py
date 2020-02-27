f2304cbd35c192dc35a1fc9c09623995c804be2b
dswhitehouse <dave@davewhitehouse.co.uk>
2020-02-27 11:09:10 +0000

    Improved Table Functionality
    Signed-off-by: dswhitehouse <dave@davewhitehouse.co.uk>

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import dash
 import plotly.graph_objects as go
-import plotly.express as px
 import dash_core_components as dcc
 import dash_table as dt
 import dash_html_components as html
@@ -18,6 +17,8 @@ randText = html.I(className="fas fa-quote-left fa-2x fa-pull-left text-white")
 
 app = dash.Dash("__name__")
 
+## Define the Nav Bar
+
 navbar = dbc.Row(
     dbc.Col(
         dbc.NavbarSimple(
@@ -26,7 +27,7 @@ navbar = dbc.Row(
                 dbc.NavItem(dbc.NavLink("Settings", href="#")),
                 dbc.NavItem(dbc.NavLink("Logout", href='#'))
             ],
-            brand="Chatter Audit",
+            brand="Chatter Auditor",
             brand_href="#",
             color="primary",
             fluid=True,
@@ -35,11 +36,15 @@ navbar = dbc.Row(
     )
 )
 
+## Define the Time / Activity Histogram
+
 x = df['transactionTime']
-fig = go.Figure(data=[go.Histogram(x=x, xbins=dict(
-    size=604800000),
-                                   autobinx=False)])
-fig.update_layout(
+histo = go.Figure(data=[go.Histogram
+                        (x=x, xbins=dict
+                        (size=604800000),
+                         autobinx=False)
+                        ])
+histo.update_layout(
     xaxis_title_text='Query Date',
     yaxis_title_text='Number of Queries',
     bargap=0.1,
@@ -49,45 +54,57 @@ fig.update_layout(
 bighist = dbc.Row(
     dbc.Col(
         dcc.Graph(
-            figure=fig,
+            figure=histo,
             id="bighist",
         )
-        , width={"size": 12}
+        , width={"size": 10, 'offset': 1}
     )
 )
 
-histo = dbc.Row(
-    [
-        # dbc.Col(
-        #     html.Div(
-        #         html.Img(src='data:image/png;base64,{}'.format(logo), height=100)
-        #     ),
-        #     width={"size": 2, "offset": 1}
-        # ),
-        dbc.Col(
-            dcc.Graph(
-                id="main-histogram",
-                figure=px.histogram(df, x=df['transactionTime'],
-                                    color=df['username'],
-                                    labels={"transactionTime": "Query Time",
-                                            "username": "User"}),
-            )
-            , width={"size": 12})
-    ]
-)
+## Define the Data Table
 
 dtable = dbc.Row(
     dbc.Col(
         dt.DataTable(
+            style_data={
+                'whiteSpace': 'normal',
+            },
+            style_cell={
+                'overflow': 'hidden',
+                'textOverflow': 'clip',
+                'maxWidth': 0,
+                'textAlign': 'left',
+            },
+            style_table={
+                'maxHeight': '600px',
+            },
+
+            style_cell_conditional=[
+                {'if': {'column_id': 'id'},
+                 'width': '10%'},
+                {'if': {'column_id': 'username'},
+                 'width': '15%'},
+                {'if': {'column_id': 'platform'},
+                 'width': '25%'},
+                {'if': {'column_id': 'authorizationID'},
+                 'width': '25%'},
+            ],
+
             id="dtable",
-            columns=[{"name": i, "id": i} for i in df.columns if not re.search('lat.*|long.*', i)],
+            columns=[{"name": i, "id": i} for i in df.columns if
+                     not re.search('^lat.*|^long.*|^polygon|^usernames|^start|^end', i)],
             data=df.to_dict('records'),
             page_action="native",
-            page_size=10
+            page_size=10,
+            sort_action="native",
+            row_selectable='single',
+            fixed_rows={'headers': True, 'data': 0},
         )
-        , width={'size': 10, "offset": 1}
+        , width={'size': 10, 'offset': 1},
     )
 )
+
+## Define the User Dropdown
 
 drop1 = dbc.Row(
     dbc.Col(
@@ -96,11 +113,27 @@ drop1 = dbc.Row(
             multi=True,
             placeholder="Select Users to Filter by"
         )
-        , width={'size': 4}
+        , width={'size': 4, 'offset': 1}
     )
 )
 
-app.layout = html.Div([navbar, bighist, drop1, dtable])
+## Define the platform Pie Chart
+
+pie = go.Figure
+
+platring = dbc.Row(
+    dbc.Col(
+        dcc.Graph(
+            figure=pie,
+            id="platring",
+        )
+        , width={"size": 10, 'offset': 1}
+    )
+)
+
+app.layout = html.Div(
+    [navbar, bighist, drop1, dtable],
+)
 
 if __name__ == "__main__":
     app.run_server(debug=True)
