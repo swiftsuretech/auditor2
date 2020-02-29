2c22c34305c2b6a1d5ae28a92562401b30f3a5dd
Dave Whitehouse <dave@davewhitehouse.co.uk>
2020-02-29 00:19:37 +0000

    Added Day Graph

--- app.py
+++ app.py
@@ -8,16 +8,33 @@ import base64
 import pandas as pd
 import plotly.io as pio
 import re
-from dash.dependencies import Input, Output, State
 import plotly.express as px
 
 pio.templates.default = "seaborn"
 logo = base64.b64encode(open("assets/logos/cii-logo.png", 'rb').read()).decode('ascii')
-df = pd.read_csv("./testData/chatter.csv")
+df = pd.read_csv("./testData/chatter.csv", parse_dates=['transactionTime'])
+df['dayOfWeek'] = df['transactionTime'].dt.day_name()
 users = df.username.unique()
 platforms = df.platform.unique()
-
+diagMargins = dict(t=10, b=10, l=10, r=10)
+x = df['transactionTime']
 app = dash.Dash("__name__")
+app.title = "Chatter Auditor"
+sp = html.Br()
+
+title = dbc.Row(
+    [
+        html.H1(html.Pre("   ")),
+        html.I(className="fal fa-user-chart fa-lg",
+               style={"font-size": "65px"}),
+        html.H1(html.Pre("[Main Dashboard]", className='body',
+                         style={"color": "#777777",
+                                'margin-top': '0.4rem'
+                                }
+                         )
+                )
+    ]
+)
 
 ## Define the Nav Bar
 
@@ -25,8 +42,8 @@ navbar = dbc.Row(
     dbc.Col(
         dbc.NavbarSimple(
             children=[
-                dbc.NavItem(dbc.NavLink(html.I(className="fal fa-cogs fa-lg text-white"), href="#")),
-                dbc.NavItem(dbc.NavLink(html.I(className="fal fa-sign-out-alt fa-lg text-white"), href="#")),
+                dbc.NavItem(dbc.NavLink(html.I(className="fal fa-cogs fa-lg"), href="#")),
+                dbc.NavItem(dbc.NavLink(html.I(className="fal fa-sign-out-alt fa-lg"), href="#")),
             ],
             brand="Chatter Auditor",
             brand_href="#",
@@ -37,38 +54,87 @@ navbar = dbc.Row(
     )
 )
 
-## Define the Time / Activity Histogram
+## Define Activity Histogram
 
-x = df['transactionTime']
-histo = go.Figure(data=[go.Histogram
-                        (x=x, xbins=dict
-                        (size=604800000),
-                         autobinx=False)
-                        ])
-histo.update_layout(
+firstDate = df.transactionTime.min()
+lastDate = df.transactionTime.max()
+
+spread = (lastDate - firstDate).days // 7
+
+hist2 = px.histogram(df, x="transactionTime", color="username", nbins=spread)
+hist2.update_layout(
+    height=350,
     xaxis_title_text='Query Date',
     yaxis_title_text='Number of Queries',
     bargap=0.1,
-    barmode='stack'
+    barmode='stack',
+    margin=diagMargins,
+    legend_title="Operator"
+)
+
+histDay = px.histogram(df, y=df["dayOfWeek"], orientation='h',
+                       labels={'Monday': 'Mon', 'Tuesday': 'Tue',
+                               'Wednesday': 'Wed', 'Thursday': 'Thu',
+                               'Friday': 'Fri', 'Saturday': 'Sat',
+                               'Sunday': 'Sun'})
+
+histDay.update_layout(
+    height=350,
+    xaxis_title_text='Day of Week',
+    yaxis_title_text='Number of Queries',
+    bargap=0.3,
+    barmode='stack',
+    margin=diagMargins,
+    yaxis=dict(categoryorder='array',
+               categoryarray=['Sunday', 'Saturday', 'Friday', 'Thursday', 'Wednesday',
+                              'Tuesday', 'Monday'])
 )
 
 bighist = dbc.Row(
     dbc.Col(
         dbc.Card(
             [
-                dbc.CardHeader("Queries by Week"),
+                dbc.CardHeader(
+                    [
+                        html.I(className="fal fa-lg fa-chart-bar"),
+                        html.I(" Queries by Week")
+                    ]
+                ),
                 dbc.CardBody(
                     [
                         dcc.Graph(
-                            figure=histo,
+                            figure=hist2,
                             id="bighist",
-
                         )
                     ]
                 )
             ]
         )
-        , width={"size": 10, 'offset': 1}
+        , width={"size": 12}
+    )
+)
+
+littlehist = dbc.Row(
+    dbc.Col(
+        dbc.Card(
+            [
+                dbc.CardHeader(
+                    [
+                        html.I(className="fal fa-lg fa-chart-bar"),
+                        html.I(" Queries by Week")
+                    ]
+                ),
+                dbc.CardBody(
+                    [
+                        dcc.Graph(
+                            figure=histDay,
+                            id="histDay",
+                        )
+                    ]
+                )
+            ]
+        )
+        , width={"size": 4}
     )
 )
 
@@ -78,7 +144,12 @@ dtable = dbc.Row(
     dbc.Col(
         dbc.Card(
             [
-                dbc.CardHeader("Query Table"),
+                dbc.CardHeader(
+                    [
+                        html.I(className="fal fa-lg fa-table"),
+                        html.I(" Queries Table")
+                    ]
+                ),
                 dbc.CardBody(
                     dt.DataTable(
                         style_data={
@@ -89,7 +160,6 @@ dtable = dbc.Row(
                         style_table={
                             'maxHeight': '600px',
                         },
-
                         style_cell_conditional=[
                             {'if': {'column_id': 'id'},
                              'width': '10%'},
@@ -100,7 +170,6 @@ dtable = dbc.Row(
                             {'if': {'column_id': 'authorizationID'},
                              'width': '25%'},
                         ],
-
                         id="dtable",
                         columns=[{"name": i, "id": i} for i in df.columns if
                                  not re.search('^lat.*|^long.*|^polygon|^usernames|^start|^end', i)],
@@ -109,11 +178,11 @@ dtable = dbc.Row(
                         page_size=10,
                         sort_action="native",
                     )
+                    , style={'margin-top': '0rem'}
                 )
             ]
-
         )
-        , width={'size': 10, 'offset': 1},
+        , width={'size': 12},
     )
 )
 
@@ -139,7 +208,7 @@ drop1 = dbc.Row(
                     )
                 ]
             )
-            , width={'size': 3, 'offset': 1}
+            , width={'size': 4}
         ),
         dbc.Col(
             dbc.Card(
@@ -159,47 +228,133 @@ drop1 = dbc.Row(
                     )
                 ]
             )
-            , width={'size': 3}
-        )
+            , width={'size': 4}
+        ),
+        dbc.Col(
+            dbc.Card(
+                [
+                    dbc.CardHeader(
+                        [
+                            html.I(className="fal fa-filter fa-lg text-black"),
+                            html.I("  Filter by IP Address"),
+                        ]
+                    ),
+                    dbc.CardBody(
+                        dcc.Dropdown(
+                            options=[{"label": i, "value": i} for i in users],
+                            multi=True,
+                            placeholder="Select IP Address to Filter by"
+                        )
+                    )
+                ]
+            )
+            , width={'size': 4}
+        ),
     ]
 )
 
 ## Define the platform Pie Chart
 
-# pie = go.Figure(
-#     data=[go.Pie(labels=df.platform.unique(), values=[10,2,4,5,16,7,43], hole=0.65)],
-# )
 
+pieplat = px.pie(df, names='platform', hole=0.6)
+pieplat.update_layout(
+    margin=diagMargins,
+)
 
-pie = px.pie(df, names='platform')
-
+pieuser = px.pie(df, names='username', hole=0.6)
+pieuser.update_layout(
+    margin=diagMargins,
+)
 
 platring = dbc.Row(
-    dbc.Col(
-        dbc.Card(
-            [
-                dbc.CardHeader(
-                    [
-                        html.I(className="fal fa-lg fa-photo-video"),
-                        html.I("&nbsp;&nbsp;&nbsp;Platform")
-                    ]
-                ),
-                dbc.CardBody(
-                    dcc.Graph(
-                        figure=pie,
-                        id="platring",
-                        style={'height': 350, 'padding': 0},
+    [
+        dbc.Col(
+            dbc.Card(
+                [
+                    dbc.CardHeader(
+                        [
+                            html.I(className="fal fa-lg fa-photo-video"),
+                            html.I(" Platform")
+                        ]
                     ),
-                    style={'margin-top': '0rem'},
-                )
-            ]
-        )
-        , width={"size": 4, "offset": 1}
-    )
+                    dbc.CardBody(
+                        dcc.Graph(
+                            figure=pieplat,
+                            id="platring",
+                            style={'height': 250, 'padding': 0},
+                            config={
+                                'displayModeBar': False}
+                        ),
+                        style={'margin-top': '0rem'},
+                    )
+                ]
+            )
+            , width={"size": 4}
+        ),
+        dbc.Col(
+            dbc.Card(
+                [
+                    dbc.CardHeader(
+                        [
+                            html.I(className="fal fa-lg fa-users"),
+                            html.I(" Operators")
+                        ]
+                    ),
+                    dbc.CardBody(
+                        dcc.Graph(
+                            figure=pieuser,
+                            id="userring",
+                            style={'height': 250, 'padding': 0},
+                            config={
+                                'displayModeBar': False}
+                        ),
+                        style={'margin-top': '0rem'},
+                    )
+                ]
+            )
+            , width={"size": 4}
+        ),
+        dbc.Col(
+            dbc.Card(
+                [
+                    dbc.CardHeader(
+                        [
+                            html.I(className="fal fa-lg fa-chart-bar"),
+                            html.I(" Queries by Week Day")
+                        ]
+                    ),
+                    dbc.CardBody(
+                        [
+                            dcc.Graph(
+                                figure=histDay,
+                                id="histogramDay",
+                                style={'height': 250, 'padding': 0},
+                                config={
+                                    'displayModeBar': False}
+                            )
+                        ],
+                        style={'margin-top': '0rem'},
+                    )
+                ]
+            )
+            , width={"size": 4}
+        ),
+    ]
 )
 
 app.layout = html.Div(
-    [navbar, html.Br(), bighist, html.Br(), platring, html.Br(), drop1, html.Br(), dtable],
+    children=[
+        navbar,
+        html.Div(
+            [title],
+            style={'width': '100%', "padding": "10px", "background-color": "#F7F7F7"}
+        ),
+        html.Div(
+            [sp, drop1, sp, bighist, sp, platring, sp, dtable, sp],
+            style={'width': '85%', 'margin': 'auto'}
+        )
+    ]
+    , style={'overflow-x': 'hidden'}
 )
 
 if __name__ == "__main__":
